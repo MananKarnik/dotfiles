1a65fe4fdf23a2b42bd537a6d9ca16eac61975ab
Manan Karnik <manankarnik03@gmail.com>
2022-05-03 13:04:42 +0530

    Added alias to display images

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,7 @@
 alias ls="lsd --group-dirs first"
 alias ll="ls -alh"
+alias icat="kitty +kitten icat --align=left"
+
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
